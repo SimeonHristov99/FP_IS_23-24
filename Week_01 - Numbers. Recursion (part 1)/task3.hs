9a1802bdf0a3d9f5d226261106588433354e7ff5
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-02-25 09:09:00 +0200

    Prepare for solving in week 2.

--- Week_01 - Numbers. Recursion (part 1)/task3.hs
+++ Week_01 - Numbers. Recursion (part 1)/task3.hs
@@ -1,3 +1,19 @@
+{-
+The first few numbers of the Fibonacci sequence are:
+`0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...` and so on.
+Define two functions `fibRec` and `fibIter`
+that return the number at the `i`th index.
+
+**Acceptance criteria:**
+
+1. All tests pass.
+2. Parameter validation is performed.
+3. `fibRec` creates a linearly recursive process.
+4. `fibIter` creates a linearly iterative process.
+5. Explain the wildcard symbol (`_`).
+6. Explain pattern matching.
+-}
+
 main :: IO()
 main = do
     print $ fibRec 11 == 89
