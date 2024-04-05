257a6e99b866727aac710913b3c7c983e11bc7f3
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-04-05 10:01:10 +0300

    Add soltuions.

--- Week_07 - Preparation for Test 1 (part 2)/task1.hs
+++ Week_07 - Preparation for Test 1 (part 2)/task1.hs
@@ -10,4 +10,26 @@ main = do
     print $ primesProd 12 == 6
     print $ primesProd 1200 == 200560490130
 
+    print $ primesProd' 9 == 2
+    print $ primesProd' 12 == 6
+    print $ primesProd' 1200 == 200560490130
 
+primesProd' :: Int -> Int
+primesProd' x = helper 2
+ where
+    helper d
+     | d * d >= x = 1
+     | isPrime d = d * helper (d + 1)
+     | otherwise = helper (d + 1)
+
+primesProd :: Int -> Int
+primesProd x = helper 2 (sqrt $ fromIntegral x)
+ where
+    helper :: Double -> Double -> Int
+    helper d limit
+     | d >= limit = 1
+     | isPrime $ round d = round d * helper (d + 1) limit
+     | otherwise = helper (d + 1) limit
+
+isPrime :: Int -> Bool
+isPrime n = [1, n] == filter (\ d -> mod n d == 0) [1 .. n]
