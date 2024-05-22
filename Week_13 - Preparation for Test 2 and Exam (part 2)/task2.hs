842c1c0e63319672bcfea30c2f0b9f89f7c78bf4
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-05-22 22:36:19 +0300

    Add solutions.

--- Week_13 - Preparation for Test 2 and Exam (part 2)/task2.hs
+++ Week_13 - Preparation for Test 2 and Exam (part 2)/task2.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 main :: IO()
 main = do
     print $ solve ["abode","ABc","xyzD"] == [4,3,1]
@@ -5,4 +7,5 @@ main = do
     print $ solve ["IAMDEFANDJKL","thedefgh","xyzDEFghijabc"] == [6,5,7]
     print $ solve ["encode","abc","xyzD","ABmD"] == [1, 3, 1, 3]
 
-
+solve :: [String] -> [Int]
+solve = map (length . filter (\ (x, y) -> x == y) . zip ['a' .. ] . map toLower)
--- Week_13 - Preparation for Test 2 and Exam (part 2)/task4.hs
+++ Week_13 - Preparation for Test 2 and Exam (part 2)/task4.hs
@@ -22,4 +22,16 @@ main = do
     print $ controller "P......P..OP..P..." == "123455543233334555"
     print $ controller "..P...O....." == "001234321000"
 
-
+controller :: String -> String
+controller input = helper input 0 0 0
+ where
+    helper "" _ _ _ = ""
+    helper ('O':xs) current_pos po prev_po = (show $ -po + current_pos) ++ helper xs (-po + current_pos) (-po) prev_po
+    helper ('P':xs) current_pos po prev_po
+     | current_pos >= 5 = (show $ current_pos - 1) ++ helper xs (current_pos - 1) (-1) prev_po
+     | 0 < current_pos && current_pos < 5 && po == 0 = (show $ current_pos + prev_po) ++ helper xs (current_pos + prev_po) prev_po prev_po
+     | 0 < current_pos && current_pos < 5 = (show current_pos) ++ helper xs current_pos 0 po
+     | otherwise = (show $ current_pos + 1) ++ helper xs (current_pos + 1) 1 prev_po
+    helper (_:xs) current_pos po prev_po
+     | current_pos <= 0 || 5 <= current_pos = (show current_pos) ++ helper xs current_pos 0 po
+     | otherwise = (show $ current_pos + po) ++ helper xs (current_pos + po) po prev_po
