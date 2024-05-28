e5fa81bbbd0d63a53b9ed613b9981a08e2492e89
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-05-28 22:36:23 +0300

    Add structure of tasks for exam 2.

--- Additional_2/task01.hs
+++ Additional_2/task01.hs
@@ -1,3 +1,9 @@
+-- Exam 2
+-- Task 1: fold
+-- Task 2: lists
+-- Task 3: trees (inspired by Stefcho!)
+-- Task 4: lists of algebraic data types
+
 main :: IO()
 main = do
     print $ prodEvens [1,2,3,4,5,6] == 48
